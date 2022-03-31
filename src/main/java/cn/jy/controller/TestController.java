92061419d38fb8e7761460d2e01495ff1c30c360
shenjiawei <2834955012@qq.com>
2022-03-31 20:52:54 +0800

    text提交 2

--- src/main/java/cn/jy/controller/TestController.java
+++ src/main/java/cn/jy/controller/TestController.java
@@ -3,7 +3,7 @@ package cn.jy.controller;
 public class TestController {
 
     public void hello() {
-        System.out.println("你好 Git  tteexxtt");
-        System.out.println("你好 Git test");
+        System.out.println("你好 Git");
+        System.out.println("你好 Git dive");
     }
 }
